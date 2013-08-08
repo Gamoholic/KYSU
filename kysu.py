3f4be1f2a1912f844836dc5afdcd912b7209cbff
Gamoholic <github.com@gamoholic.info>
2013-08-08 17:36:34 -0600

    Code cleanup. I longer assign values to multiple variables on one line. This should make debugging easier. I did not mess with initiating/clearing multiple variables on one line because that should not cause any problems.

--- kysu.py
+++ kysu.py
@@ -16,7 +16,9 @@ for arg in range(len(ARGS)):
         if ARGS[arg] == arg_dict[key]:
             op_dict[key] = True
             del_args += 1
-TEST, URL, FINAL = op_dict['test'], op_dict['url'], op_dict['final']
+TEST = op_dict['test']
+URL = op_dict['url']
+FINAL = op_dict['final']
 for val in range(del_args):
     del ARGS[0]
     
@@ -54,7 +56,8 @@ def name_replace(name, ver):
     return name.replace(name_rep, name_str)
 
 def build_dict(filenames):
-    dict_a, files = {}, [s for s in filenames for x in EXTS if s.endswith(x)]
+    dict_a = {}
+    files = [s for s in filenames for x in EXTS if s.endswith(x)]
     for s in files:
         dict_a[res('(.*)\-', s, 1)] = res('-([\d\.]*)\.\w+', s, 1)
     return dict_a
@@ -81,15 +84,18 @@ for alist in big_list:
     if ver == '':
         print '{} {} {}'.format(cur_time(), "Unable to find version number for:", name)
     else:
-        url, final = name_gen(alist[2], ver), name_gen(alist[0], ver)
+        url = name_gen(alist[2], ver)
+        final = name_gen(alist[0], ver)
         url_list.append(url)
         final_list.append(final)
-        url_dict[name], final_dict[name] = url, final
+        url_dict[name] = url
+        final_dict[name] = final
         if URL == True: 
             print '{} {} {}'.format(cur_time(), name, url)
         if FINAL == True: 
             print '{} {}'.format(cur_time(), final)
-local_dict, update_dict = build_dict(local_files), build_dict(final_list)
+local_dict = build_dict(local_files)
+update_dict = build_dict(final_list)
 c_up, c_new = 0,0
 for key in update_dict:
     down_loc = ARGS[1] + final_dict[key]
