fbca98df78101ca8d89d2010b343e838b6cb2a02
Gamoholic <github.com@gamoholic.info>
2013-08-15 22:44:01 -0600

    Woo hoo! I finally figured out how to add email reporting!

--- kysu.py
+++ kysu.py
@@ -4,11 +4,12 @@ import datetime, os, re, sys, urllib
 # Global Variables
 FH = 'http://www.filehippo.com'
 EXTS = ['.exe', '.msi', '.iso', '.zip']
+MESSAGE = ''
 ARGS = sys.argv[1:]
-arg_dict = {'test': '-test', 'url': '-url', 'final': '-final'}
-op_dict = {'test': False, 'url': False, 'final': False}
+arg_dict = {'test': '-test', 'url': '-url', 'final': '-final', 'email': '-email'}
+op_dict = {'test': False, 'url': False, 'final': False, 'email': False}
 if not ARGS:
-    print 'usage: [-test] [-url] [-final] db_file download_location'
+    print 'usage: [-test] [-url] [-final] [-email] db_file download_location'
     sys.exit(1)
 del_args = 0
 for arg in range(len(ARGS)):
@@ -19,6 +20,8 @@ for arg in range(len(ARGS)):
 TEST = op_dict['test']
 URL = op_dict['url']
 FINAL = op_dict['final']
+EMAIL = op_dict['email']
+SEND_EMAIL = False
 for val in range(del_args):
     del ARGS[0]
 if ARGS[1].endswith('/') == -1:
@@ -27,7 +30,7 @@ if ARGS[1].endswith('/') == -1:
 def make_html(url): 
     html = urllib.urlopen(url).read()
     if re.search('404 \- Not Found', html) != None:
-        log("Bad URL:", url)
+        log('WARNING', "Bad URL:", url)
     return html
   
 def name_gen(name, ver):
@@ -74,18 +77,24 @@ def res(regex, string, x):
     if a != None:
         return a.group() if x ==0 else a.group(x)
     else:
-        log("Regex failed for:", regex, string)
+        log('WARNING', "Regex failed for:", regex, string)
         return ''
         
 def cur_time():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
-def log(*args):
+def log(level, *args):
     format_string = '{}'
     for i in range(len(args)):
         format_string += ' {}'
-    print format_string.format(cur_time(), *args)
-
+    log_message = format_string.format(cur_time(), *args)
+    print log_message
+    if level == 'WARNING' or level == 'NOTICE':
+        global SEND_EMAIL
+        global MESSAGE
+        SEND_EMAIL = True
+        MESSAGE += log_message + '\n'
+        
 #Main
 print datetime.datetime.now().strftime("%m-%d-%Y") #Today's date
 local_files = os.listdir(ARGS[1])
@@ -96,7 +105,7 @@ for alist in big_list:
     name = res('(.*)\-', alist[0], 1)
     ver = res(alist[3], make_html(alist[1]), 0)
     if ver == '':
-        log("Unable to find version number for:", name)
+        log('WARNING', "Unable to find version number for:", name)
     else:
         url = name_gen(alist[2], ver)
         final = name_gen(alist[0], ver)
@@ -106,11 +115,11 @@ for alist in big_list:
             url_dict[name] = url
             final_dict[name] = final
             if URL == True: 
-                log(name, url)
+                log('INFO', name, url)
             if FINAL == True: 
-                log(final)
+                log('INFO', final)
         else:
-            log("Skipped due to improper use of $:", name)
+            log('WARNING', "Skipped due to improper use of $:", name)
 local_dict = build_dict(local_files)
 update_dict = build_dict(final_list)
 counter_updated, counter_new = 0,0
@@ -122,17 +131,32 @@ for key in update_dict:
             file_to_delete = key + '-' + local_dict[key]
             for s in local_files:
                 if s.find(file_to_delete) != -1:
-                    log('Deleted', s)
+                    log('NOTICE', 'Deleted', s)
                     if TEST == False:
                         os.remove(ARGS[1] + s)
-            log('Downloading', final_dict[key])
+            log('NOTICE', 'Downloading', final_dict[key])
             if TEST == False:
                 urllib.urlretrieve(url_dict[key], download_location)
     else:
         counter_new += 1
-        log('Downloading', final_dict[key])
+        log('NOTICE', 'Downloading', final_dict[key])
         if TEST == False:
             urllib.urlretrieve(url_dict[key], download_location)
-log('Number of updates:', counter_updated)
-log('Number of new files:', counter_new)
+log('INFO', 'Number of updates:', counter_updated)
+log('INFO', 'Number of new files:', counter_new)
 print
+
+#Email Logger
+if EMAIL == True and SEND_EMAIL == True:
+    import smtplib
+    param_list = [s.strip() for s in open('email_params.txt', 'rU').readlines()]
+    sender = param_list[0]
+    receiver = param_list[1]
+    username = param_list[2]
+    password = param_list[3]
+    server = param_list[4]
+    message = ("From: KYSU <%s>\nTo: <%s>\nSubject: Message from KYSU\n\n%s" % (sender, receiver, MESSAGE))
+    email = smtplib.SMTP(server)
+    email.starttls()
+    email.login(username, password)
+    email.sendmail(sender, receiver, message)
